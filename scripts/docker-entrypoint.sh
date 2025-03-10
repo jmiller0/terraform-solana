df76a8778665e360c4ee217ee284da2ce9daf44f
jmiller0 <github@miller.center>
2025-03-09 23:50:49 -0400

    Remove dupe, logging

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Skip terraform init if we're in CI or if SKIP_INIT is set
 if [[ -z "${CI}" && -z "${SKIP_INIT}" && ! -d ".terraform" ]]; then
-    echo "Initializing Terraform..."
+    echo "Initializing Terraform From Entrypoint CI: ${CI} SKIP_INIT: ${SKIP_INIT}..."
     terraform init
 fi
 
